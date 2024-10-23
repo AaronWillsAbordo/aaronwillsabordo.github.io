aae2b464a73519ae37441e67ca572a7981633a56
Ren <aaronwills.abordo@gmail.com>
2024-10-23 23:56:14 +0800

    added content-box

--- src/Profile.js
+++ src/Profile.js
@@ -10,29 +10,31 @@ export default function Profile() {
 
     return (
         <div className = "layoutWhite">
-            <h1 style={{ textAlign: 'left' }}>Hi, I'm Aaron 👋</h1>
-            <p>
-                I’m a <strong>Software Engineer</strong> with expertise in <strong>Python</strong>, 
-                <strong>.NET</strong>, <strong>React</strong>, and <strong>data analytics</strong>, 
-                dedicated to building efficient, scalable solutions. 
-                I’m also passionate about creating impactful software 
-                and exploring <strong>AI technologies</strong> in fast-paced, collaborative environments.
-            </p>
-            
-            <a href={linkMap.urlLink} target="_blank" rel="noopener noreferrer" className="location-info">
-                <img src={imgLocation.imgSource} alt="location" />
-                <p>Taguig City, Philippines</p>
-            </a>
+            <div className="content-box">
+                <h1 style={{ textAlign: 'left' }}>Hi, I'm Aaron 👋</h1>
+                <p>
+                    I’m a <strong>Software Engineer</strong> with expertise in <strong>Python</strong>
+                    , <strong>.NET</strong>, <strong>React</strong>, and <strong>data analytics</strong>, 
+                    dedicated to building efficient, scalable solutions. 
+                    I’m also passionate about creating impactful software 
+                    and exploring <strong>AI technologies</strong> in fast-paced, collaborative environments.
+                </p>
+                
+                <a href={linkMap.urlLink} target="_blank" rel="noopener noreferrer" className="location-info">
+                    <img src={imgLocation.imgSource} alt="location" />
+                    <p>Taguig City, Philippines</p>
+                </a>
 
-            <div className='layout-sidebyside'>
-                <a href={imgLinkedIn.imgUrl} target="_blank" rel="noopener noreferrer">
-                    <img src={imgLinkedIn.imgSource} alt="LinkedIn" />
-                </a>
-                <a href={imgGitHub.imgUrl} target="_blank" rel="noopener noreferrer">
-                    <img src={imgGitHub.imgSource} alt="GitHub" />
-                </a>
+                <div className='layout-sidebyside'>
+                    <a href={imgLinkedIn.imgUrl} target="_blank" rel="noopener noreferrer">
+                        <img src={imgLinkedIn.imgSource} alt="LinkedIn" />
+                    </a>
+                    <a href={imgGitHub.imgUrl} target="_blank" rel="noopener noreferrer">
+                        <img src={imgGitHub.imgSource} alt="GitHub" />
+                    </a>
+                </div>
+
             </div>
-
         </div>
     );
 }
