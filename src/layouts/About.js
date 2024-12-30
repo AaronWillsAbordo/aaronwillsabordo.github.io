737b69cb227a809ebd3503329112c07c09706e5d
Ren <aaronwills.abordo@gmail.com>
2024-12-30 19:50:04 +0800

    app complete

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -12,11 +12,11 @@ const About = forwardRef((props, ref) => {
             <h2>Curious about me? Here you go:</h2>
 
             {props.isMobile ? 
-                <div className='about-mobile-container' style={{columns: '1'}}>
-                    {/* <div className="about-mobile-image-section">
+                <div className='about-mobile-container' >
+                    {/*style={{columns: '1'}} <div className="about-mobile-image-section">
                             <div id="about-rectangle" ></div>
                         </div> */}
-                    <h3 dangerouslySetInnerHTML={{ __html: dataAbout[0].about.join('') }}>
+                    <h3 style={{margin: '0'}} dangerouslySetInnerHTML={{ __html: dataAbout[0].about.join('') }}>
                     </h3>
                     <img src={imgProfile.source} alt="profile-pic" className="about-mobile-profile-image" />
                 </div>
